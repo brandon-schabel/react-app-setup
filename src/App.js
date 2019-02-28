2c2a5548c5a66eff1e05ffdc135f91c1b6bdb66e
brandon-schabel <brandonschabel1995@gmail.com>
2019-02-28 15:54:57 -0800

    added todo input

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import React, { Component } from 'react';
+import React, { useState } from "react"
+import TodoInput from './components/TodoInput/'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+function App() {
+
+  const [ todos, modifyToDos ] = useState(['study with kahaan'])
+
+  return <TodoInput />
 }
 
-export default App;
+export default App
---
+++ src/components/TodoInput/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { InputWrapper } from './styles';
+
+const index = () => {
+  return (
+    <InputWrapper>
+      <input></input>
+    </InputWrapper>
+  )
+}
+
+export default index;
